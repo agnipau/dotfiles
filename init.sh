759537e4a1ea2461e9caab7611a072f128ecc11c
Matteo Guarda <matteoguarda@protonmail.com>
2018-07-11 00:33:09 +0200

    update

--- init.sh
+++ init.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
 #
 # Script to install my dotfiles.
-# Requires GNU stow.
 
-cd dotfiles
-for i in $(ls); do stow -D $i; done
+for i in dotfiles/*; do stow -D "$i"; done
 echo "==> Starting to create symbolic links through GNU stow..."
-for i in $(ls); do stow -t $HOME $i; done
+for i in dotfiles/*; do stow -t "$HOME" "$i"; done
 echo "==> Finished!"
