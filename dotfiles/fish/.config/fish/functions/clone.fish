614daf3dbd0823490d16aef64e1d85688da6f5d4
Matteo Guarda <matteoguarda@protonmail.com>
2018-09-08 07:47:34 +0200

    update

--- dotfiles/fish/.config/fish/functions/clone.fish
+++ dotfiles/fish/.config/fish/functions/clone.fish
@@ -1,3 +1,7 @@
 function clone
-  git clone --depth 1 https://github.com/$argv[1].git
+  if test $argvs[1] = gitlab
+    git clone --depth 1 https://gitlab.com/$argv[2].git
+  else
+    git clone --depth 1 https://github.com/$argv[1].git
+  end
 end
