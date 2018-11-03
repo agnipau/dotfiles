13a923a9be96d581bd70c188eefcf7bb6663e13e
Matteo Guarda <matteoguarda@protonmail.com>
2018-11-03 23:26:09 +0100

    update

---
+++ dotfiles/gnome/make-backup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#
+# Save the dconf settings in a ini file.
+
+dconf dump / >dconf-settings.ini
---
+++ dotfiles/gnome/recover-backup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#
+# Recover the dconf settings from an ini file.
+
+dconf-settings.ini >dconf load /
