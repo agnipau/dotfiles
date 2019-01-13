d095b914c625eccb9e393beb91d0c8bc07814c83
Matteo Guarda <matteoguarda0@gmail.com>
2019-01-13 03:18:46 +0100

    update

--- deepin-setup/fish/.config/fish/config.fish
+++ deepin-setup/fish/.config/fish/config.fish
@@ -78,7 +78,7 @@ alias du    'du -h'
 alias mkdir 'mkdir -p'
 alias pq    'pqiv -i --box-colors=$color7:$color13 --bind-key="@MONTAGE { h { montage_mode_shift_x(-1) } }" --bind-key="@MONTAGE { j { montage_mode_shift_y(1) } }" --bind-key "@MONTAGE { k { montage_mode_shift_y(-1) } }" --bind-key "@MONTAGE { l { montage_mode_shift_x(1) } }" --bind-key="j { goto_file_relative(-1) }" --bind-key="k { goto_file_relative(1) }" --bind-key="h { goto_file_relative(-1) }" --bind-key="l { goto_file_relative(1) }" --bind-key="d { command(rm $1) }" $argv'
 
-# Start tmux automatically whenever a new terminal instance is opened.
-test -z $TMUX;
-  and test $DISPLAY;
-  and tmux -2
+## Start tmux automatically whenever a new terminal instance is opened.
+#test -z $TMUX;
+#  and test $DISPLAY;
+#  and tmux -2
